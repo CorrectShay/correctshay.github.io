d1c38ba2a2a379cb22fd027920659cdcfb6c6613
CorrectShay <55422709+CorrectShay@users.noreply.github.com>
2020-11-29 13:05:30 +1300

    Create init-firebase.js

---
+++ js/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD2J1l1KYKDfJIDgQw-ojeLvqWA_wmFZoI",
+  authDomain: "kronos-web-dispo.firebaseapp.com",
+  databaseURL: "https://kronos-web-dispo.firebaseio.com",
+  projectId: "kronos-web-dispo",
+  storageBucket: "kronos-web-dispo.appspot.com",
+  messagingSenderId: "927239941362",
+  appId: "1:927239941362:web:c85f06704c1d2be9b9746b"
+};
+
+firebase.initializeApp(firebaseConfig);
